da166a5456ded7bfad225c8484b020fc9e1ca122
Sophia <soamaka@gmail.com>
2023-06-26 08:54:24 -0700

    new

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-# 101-safe_function.py
-
 import sys
+
+"""
+File_name: 101-safe_function.py
+Size: undefined
+"""
 
 
 def safe_function(fct, *args):
-    """Executes a function safely.
 
-    Args:
-        fct: The function to execute.
-        args: Arguments for fct.
-
-    Returns:
-        If an error occurs - None.
-        Otherwise - the result of the call to fct.
+    """
+    # Write a function that executes a function safely.
+    # VARIABLE(" "):
+    # mafic_calculation(int): ByteCode -> Python #4
+    # Returns: the result of the function,
+    # Otherwise, returns None if something happens during the...
+    # ....function and prints in stderr the error precede by Exception:
     """
     try:
-        result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        """We use a 'try'-except' block to execute the provided function 'fct'
+        with the given arguments '*args'."""
+        total_sum = fct(*args)
+        """If the function execution is successful, total_sum is returned"""
+        return total_sum
+    except Exception as error:
+        """The error message is printed out using the below function"""
+        sys.stderr.write("Exception: {}\n".format(error))
+        """ returns None to indicate something went wrong......
+        .......during the execution"""
+        return None
--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,16 +1,30 @@
-#!/usr/bin/python3i
-# 102-magic_calculation.py
+#!/usr/bin/python3
+
+"""
+File_name: 102-magic_calculation.py
+"""
 
 
 def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
+
+    """
+    # that does exactly the same as the following Python bytecode
+    # VARIABLE(" "):
+    # mafic_calculation(int): ByteCode -> Python #4
+    # Returns: Always 0. (Success)
+    """
+    total_sum = 0
+    for x in range(1, 3):
+        """The byte code represent a Loop that iterates over range '(1, 4)"""
         try:
-            if i > a:
-                raise Exception('Too far')
-            else:
-                result += a ** b / i
-        except:
-            result = b + a
+            if x > a:
+                """Inside this Loop, there is a comparison 'if x > a'"""
+                """And if it's true, an exception is raised,
+                Otherwise, the calculation below"""
+                raise Exception('Too Far')
+            total_sum += (a ** b) / x
+        except BaseException:
+            total_sum = b + a
             break
-    return (result)
+        """ Finally, the 'total_sum' is returned"""
+    return total_sum
