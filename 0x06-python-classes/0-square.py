fb7415eb2481ab9332b7faa127080d9093881e08
Sophia <soamaka@gmail.com>
2023-06-28 16:56:23 -0700

    tasks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+File_name: 0-square.py
+"""
+
+
+class Square:
+
+    """
+    # Write an empty class Square that defines a square:
+    # You are not allowed to import any module
+    # VARIABLE(" "):
+    # class(Square): My first square
+    # Return: Always 0. (Success)
+    """
+    """ This class does not have any attributes or methods defined yet"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+File_name: 1-square.py
+"""
+
+
+class Square:
+
+    """
+    # Write a class Square that defines a square by: (based on 0-square.py)
+    # Private instance attribute: size
+    # Instantiation with size (no type/value verification)
+    # VARIABLE(" "):
+    # class(Square): Square with size
+    # Return: Always 0. (Success)
+    """
+    """ Here, the '__init__' method is used as the constructor."""
+    def __init__(self, size):
+        """ It takes a parameter 'size' and initializes the private instance...
+        ...attribute '__size' with the provided value"""
+        self.__size = sizei
