cda86798ba81080e0a1f6bb3af1452878300c5aa
Sophia <soamaka@gmail.com>
2023-08-17 19:11:02 -0700

    task

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Creates the database hbtn_Oc_O if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Deletes the database hbtn_0c_0 from the server if it exists without any errors
+DROP DATABASE IF EXISTS hbtn_0c_0;
